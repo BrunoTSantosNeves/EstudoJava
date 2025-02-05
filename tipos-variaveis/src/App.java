374c8c1c62b46c6a6800626f3b286390fac8ac93
bidbibb <tonetti583@gmail.com>
2025-02-05 16:08:59 -0300

    Tipos e variaveis

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,5 +1,32 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Tipos primitivos
+        int idade = 10;
+        double altura = 1.75;
+        char sexo = 'M';
+        boolean ativo = true;
+
+        // Tipos de referência
+        String nome = "João";
+        Integer idade2 = 10;
+        Double altura2 = 1.75;
+        Character sexo2 = 'M';
+        Boolean ativo2 = true;
+
+        // Imprimindo os valores
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Sexo: " + sexo);
+        System.out.println("Ativo: " + ativo);
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade2);
+        System.out.println("Altura: " + altura2);
+        System.out.println("Sexo: " + sexo2);
+        System.out.println("Ativo: " + ativo2);
+
+        // Constantes
+        final double VALOR_PI = 3.1415;
+
     }
 }
