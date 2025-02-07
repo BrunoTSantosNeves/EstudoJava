647c950432a1df3380c4cfb6eb413b13a8d6c6cf
bidbibb <tonetti583@gmail.com>
2025-02-07 19:08:33 -0300

    Condição ternaria

--- Controle-de-Fluxo/src/ResultadoEscolarTernario.java
+++ Controle-de-Fluxo/src/ResultadoEscolarTernario.java
@@ -1,4 +1,9 @@
 public class ResultadoEscolarTernario {
+
+    /*
+     * Mostrando uma estrutura condicional ternária
+     * O sintaxe é: condição ? valor_se_verdadeiro : valor_se_falso
+     */
     public static void main(String[] args) {
         double nota = 6.0;
         String resultado = nota >= 7.0 ? "Aprovado" : nota >= 5.0 && nota < 7.0 ? "Recuperação" : "Reprovado";
