3a309be5094b02b49b754e3d8bbd432b9ee96cab
bidbibb <tonetti583@gmail.com>
2025-02-07 18:17:11 -0300

    Controle de fluxo, mostrando uma estrutura condicional composta e encadeada em dos exemplos de exercicios diferentes

--- Controle-de-Fluxo/src/ResultadoEscolar.java
+++ Controle-de-Fluxo/src/ResultadoEscolar.java
@@ -6,7 +6,7 @@ public class ResultadoEscolar {
         double nota = 7.0;
         if (nota >= 7.0) {
             System.out.println("Aprovado");
-        } else if (nota >= 5.0) {
+        } else if (nota >= 5.0 && nota < 7.0) {
             System.out.println("Recuperação");
         } else {
             System.out.println("Reprovado");
