fe7c7224a8997185fd097231010ca7d3c380262d
bidbibb <tonetti583@gmail.com>
2025-02-07 19:05:49 -0300

    Condição ternaria

---
+++ Controle-de-Fluxo/src/ResultadoEscolarTernario.java
@@ -0,0 +1,7 @@
+public class ResultadoEscolarTernario {
+    public static void main(String[] args) {
+        double nota = 6.0;
+        String resultado = nota >= 7.0 ? "Aprovado" : nota >= 5.0 && nota < 7.0 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
