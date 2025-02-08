9f6688043c1dc82eea33b0e356ed775dbec6ef08
bidbibb <tonetti583@gmail.com>
2025-02-08 20:58:59 -0300

    Estudando estruturas de repetição em java

--- Estrutura-Repetição/src/ForArray.java
+++ Estrutura-Repetição/src/ForArray.java
@@ -1,9 +1,29 @@
 public class ForArray {
+
+    /*
+     * Exemplo de um Array com for
+     * numeros é um array de inteiros que contém os valores de 1 a 10.
+     * O for percorre o array e imprime o valor de cada elemento.
+     */
+
     public static void main(String[] args) {
         int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        String[] nomes = {"Alice", "Bob", "Charlie", "David", "Eve"};
 
         for (int i = 0; i < numeros.length; i++) {
             System.out.println("Valor de i: " + numeros[i]);
+
+            /*
+             * O for também pode ser utilizado para percorrer arrays de Strings.
+             * nomes é um array de Strings que contém os nomes Alice, Bob, Charlie, David e Eve.
+             * O for percorre o array e imprime o valor de cada elemento.
+             */
+
+       // for (int j = 0; j < nomes.length; j++) {
+         //   System.out.println("Nome: " + nomes[j]);
+
+        
         }
     }
 }
+//}
