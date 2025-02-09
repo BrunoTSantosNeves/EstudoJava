0528bad8e8662f08ff6dad95effff70784bdefef
bidbibb <tonetti583@gmail.com>
2025-02-08 23:20:55 -0300

    Incluindo o break e o continue em estruturas de repetição

---
+++ Estrutura-Repetição/src/loopWhile.java
@@ -0,0 +1,16 @@
+public class loopWhile {
+
+    /*
+     * O while é uma estrutura de repetição que permite executar um bloco de código
+     * várias vezes. A estrutura do while é composta por uma condição de continuação
+     * e um bloco de código.
+     */
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.println("Valor de i: " + i);
+            i++;
+        }
+    }
+}
